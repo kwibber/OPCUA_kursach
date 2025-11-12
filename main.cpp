c29c230f5b4e43de0156f9cbf6569442fff4d3bc
kwibber <vadim.popov0660@gmail.com>
2025-11-12 14:18:13 +0300

    123

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <fstream>
 
+
 int main(){
     int n;
 }
---
+++ test1.cpp
@@ -0,0 +1,34 @@
+#include <open62541/client.h>
+#include <open62541/client_highlevel.h>
+#include <open62541/plugin/log_stdout.h>
+#include <stdlib.h>
+
+int main(void) {
+    UA_Client *client = UA_Client_new();
+    UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+    UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:4840");
+    if(retval != UA_STATUSCODE_GOOD) {
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "The connection failed with status code %s", UA_StatusCode_name(retval));
+        UA_Client_delete(client);
+        return EXIT_FAILURE;
+    }
+    /* Read the value attribute of the node. UA_Client_readValueAttribute is a  wrapper for the raw read service available as UA_Client_Service_read. */
+    UA_Variant value; /* Variants can hold scalar values and arrays of any type */
+    UA_Variant_init(&value);
+
+    /* NodeId of the variable holding the current time */
+    const UA_NodeId nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
+    retval = UA_Client_readValueAttribute(client, nodeId, &value);
+
+    if(retval == UA_STATUSCODE_GOOD &&
+       UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
+        UA_DateTime raw_date = *(UA_DateTime *) value.data;
+        UA_DateTimeStruct dts = UA_DateTime_toStruct(raw_date);
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,"date is: %u-%u-%u %u:%u:%u.%03u",dts.day, dts.month, dts.year, dts.hour,dts.min, dts.sec, dts.milliSec);
+    } else {
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,"Reading the value failed with status code %s",UA_StatusCode_name(retval));
+    }
+    UA_Variant_clear(&value); /* Clean up */
+    UA_Client_delete(client); /* Disconnects the client internally */
+    return EXIT_SUCCESS;
+}
